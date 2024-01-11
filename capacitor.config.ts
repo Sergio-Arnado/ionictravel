b64cdfc342cd7d7df942d86547ee267f9c13e061
Sergio Arnado <sergioarnado@gmail.com>
2024-01-10 22:44:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ionictravel',
+  appName: 'ionictravel',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
